873ed73a5ee89402879765629736520b5a29d798
Aria Lian <arialian0829@gmail.com>
2023-10-10 03:44:54 -0400

    RenderSystem Update

--- Lab02_EngineFoundation/RenderSystem.cpp
+++ Lab02_EngineFoundation/RenderSystem.cpp
@@ -11,6 +11,8 @@ RenderSystem::RenderSystem()
 
 RenderSystem::~RenderSystem()
 {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	std::cout << "RenderSystem Destroyed" << std::endl;
 }
 
@@ -48,17 +50,6 @@ void RenderSystem::Initialize(json::JSON& _json)
 	window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	while (true)
-	{
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		SDL_RenderPresent(renderer);
-	}
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
 	std::cout << "RenderSystem Initialized" << std::endl;
 }
 
@@ -70,6 +61,41 @@ void RenderSystem::Destroy()
 void RenderSystem::Update()
 {
 	//process the SDL_PollEvents (you don’t have to handle them)
+	bool quit = false;
+
+	while (!quit)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_WINDOWEVENT:
+				if (event.window.event == SDL_WINDOWEVENT_CLOSE)
+				{
+					quit = true;
+				}
+				break;
+
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+				std::cout << "Key Event" << std::endl;
+				break;
+
+				//case SDL_MOUSEMOTION:
+			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONUP:
+			case SDL_MOUSEWHEEL:
+				std::cout << "Mouse Event" << std::endl;
+				break;
+			}
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer); 
+
+		SDL_RenderPresent(renderer);
+	}
 	
 	std::cout << "Render System Update" << std::endl;
 }
